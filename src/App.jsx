61539c60ccbaaa30eb70f113d15ab565b041e5a2
Peeravit <peeravit42@gmail.com>
2024-01-27 14:34:35 +0700

    feat App.jsx App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import "./App.css";
+import Movies from "./data/movies";
 
 function App() {
   return (
     <div className="App">
-      <section className="movie-list-section">
-        {/* Render Movie Lists Here */}
-      </section>
+      <div className="movie-list-section">
+        <p className="Title">Movie List Section</p>
+        {Movies.map((item)=>{
+          return(
+            <div className="Movie-box">
+              <img src="{item.image}" height={100} width={102}/>
+              <section className="movie-detail">
+              <p>Title:{item.title}</p>
+              <p>Year:{item.year}</p>
+              <p>Runtime:{item.runtime}</p>
+              <p>
+                Genres:{" "}
+                {item.genres.map((genres) => (
+                  <span>{genres},</span>
+                ))}
+              </p>
+              <p>IMDB Ratings:{item.imdbRating}</p>
+              <p>IMDB Votes:{item.imdbVotes}</p>
+              </section>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
